b004e0f27ecdc8eb79118acbd179070d82a9df7b
Alex Pardoe <digital.pardoe@gmail.com>
2011-02-11 22:40:34 +0000

    Union the array (prevents duplicates), updated README.

--- lib/lessr/railtie.rb
+++ lib/lessr/railtie.rb
@@ -1,7 +1,7 @@
 class Railtie < ::Rails::Railtie
   config.before_initialize do
     if ::Rails.root.join("public/javascripts/less.min.js").exist?
-      config.action_view.javascript_expansions[:defaults] += %w(less.min)
+      config.action_view.javascript_expansions[:defaults] |= %w(less.min)
     end
   end
 end
