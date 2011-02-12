540b61ccc755df6840555a17fd33abc29f281158
Alex Pardoe <digital.pardoe@gmail.com>
2011-02-12 01:12:33 +0000

    Not overwriting the developer's configuration.

--- lib/lessr/railtie.rb
+++ lib/lessr/railtie.rb
@@ -1,5 +1,5 @@
 class Railtie < ::Rails::Railtie
-  config.before_initialize do
+  config.before_configuration do
     if ::Rails.root.join("public/javascripts/less.min.js").exist?
       config.action_view.javascript_expansions[:defaults] |= %w(less.min)
     end
